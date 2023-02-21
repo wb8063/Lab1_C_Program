27941a9e5f473840b0442d155570b1af7c70c51e
wb8063 <116230656+wb8063@users.noreply.github.com>
2023-02-21 14:39:03 -0700

    Finished lab1.c
    realized my understanding  of the rubric was wrong, changed the whole thing to fit the criteria

--- lab1.c
+++ lab1.c
@@ -2,92 +2,89 @@
 #include <stdbool.h>
 
 bool within_x_percent(float ref, int data, float x){
-        float min;
-        float max;
-        min = ref-((x*100)/ref)*ref;
-        max = ref +((x*100)/ref)*ref;
 
-        if(data>= min && data <= max){
-                if(100 >= min && 100 <= max){
-                        printf("Substance is water");
-                        return 1;
-                }
-                else if(357 >= min && 357 <= max){
-                        printf("Substance is Mercury");
-                        return 1;
-                }
-                else if(1187 >= min && 1187 <= max){
-                        printf("Substance is Copper");
-                        break;
-                        return 1;
-                }
-                else if(2193 >= min && 2193 <= max){
-                        printf("Substance is Silver");
-                        return 1;
-                }
-                else if(2660 >= min && 2660 <= max){
-                        printf("Substance is Gold");
-                        return 1;
-                }
-        }
-        else{
-        printf("Substance unknown");
-                return 0;
 
+        if(ref >= (100 - ((x/100)*100)) && ref<= (100+((x/100)*100))){ // checking if min and max are both between or equal to 100
+                printf("Substance is water\n");// if it is the substance is water
+                return 1;
         }
+        else if(ref >= (357-((x/100)*357)) && ref<= (357+((x/100)*357))){// checking if min and max are both between or equal to  357
+                printf("Substance is Mercury\n");//if it is the substance is mercury
+                return 1;
+        }
+        else if(ref >= (1187-((x/100)*1187)) && ref<= (1187+((x/100)*1187))){// checking if min and max are both between or equal to 1187
+                printf("Substance is Copper\n");//if it is the substance is copper
+                return 1;
+        }
+        else if(ref >= (2193-((x/100)*2193)) && ref<= (2193+((x/100)*2193))){// checking if min and max are both between or equal to 2193
+                printf("Substance is Silver\n");//if it is the substance is silver
+                return 1;
+        }
+        else if(ref >= (2660-((x/100)*2660)) && ref<= (2660+((x/100)*2660))){// checking if min and max are both between or equal to 2660
+                printf("Substance is Gold\n");//if it is the substance is gold
+                return 1;
+        }
+        else{//if none of the others then its unknown
+        printf("Substance unknown\n");
+        return 0;
+        }
+
         return true;
 }
 
 int main(void){
-        float ref; //refernece temperature
-        int data; //reference -y
+        float ref; //what people input
+        int data; //the percentage of ref
         float x; //error percentage
-        int y; //place holder for getting data
+        float y; //place holder for getting data
         char ans; //yes no answer holder
         printf("Observed boiling point (in deg. C)? ");
-        scanf("%f", &ref);
-        printf("You entered %.2f", ref);
+        scanf(" %f", &ref);
+        printf("You entered %.2f\n", ref);
         printf("Custom error percent? n for no (5%% default), y for yes: ");
-        scanf("%c", &ans);
-        printer(ans);
-        switch(ans){
+        scanf(" %c", &ans);
+
+        switch(ans){//switch statement for multiple answers for y, Y, n , N and anything not applicable
                 case 'n':
-                x= 5;
+                x= 5;//sets error percentage to automatically be 5%
                 y=500/ref;
                 data = ref-y;
-                within_x_percent(ref, data, x);
+                within_x_percent(ref, data, x);//calls function
                 break;
 
                 case 'N':
-                x= 5;
+                x= 5;//sets error percentage to automatically be 5%
                 y=500/ref;
-                data = ref-y;
-                within_x_percent(ref, data, x);
+                data= ref-y;
+                within_x_percent(ref, data, x);//calls fnct
                 break;
-                     case 'y':
-                printf("Enter error percent: ");
+
+                case 'y':
+                printf("Enter error percent: ");//asks for user input
                 scanf("%f", &x);
-                printf("You entered %.2f", x);
+                printf("You entered %.2f percent\n", x);//prints user input with decimals
                 y=(x*100)/ref;
                 data = ref-y;
-                within_x_percent(ref, data, x);
+                within_x_percent(ref, data, x);//calls fnct
                 break;
 
                 case 'Y':
-                printf("Enter error percent: ");
+                printf("Enter error percent: ");//asks for user input
                 scanf("%f", &x);
-                printf("You entered %.2f", x);
+                printf("You entered %.2f percent\n", x);//prints user input
                 y=(x*100)/ref;
                 data = ref-y;
-                within_x_percent(ref, data, x);
+                within_x_percent(ref, data, x);//calls fnct
                 break;
-            
-                default:
-                printf("Error: bad input");
+                 default:
+                printf("Error: bad input\n");//for anything not y, n, N, or Y
                 break;
        }
+
+
         return 0;
 }
+
 
 
 
